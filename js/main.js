9c15e9e420072f1d82733cdbef7936d38909a809
sc30gsw <haidoushi875@gmail.com>
2021-08-25 08:08:39 +0900

    準備

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    const li = document.createElement('li');
-    const color = document.querySelector('select');
-    li.textContent = `${color.value} - ${color.selectedIndex}`;
-    document.querySelector('ul').appendChild(li);
+    
   });
 }
