4c09cc37f7084ca17f3de05a3ea7df23ffb691dd
sc30gsw <haidoushi875@gmail.com>
2021-08-25 08:00:47 +0900

    セレクトボックスの場合

--- js/main.js
+++ js/main.js
@@ -2,12 +2,6 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    const li = document.createElement('li');
-    const text = document.querySelector('input');
-    li.textContent = text.value;
-    document.querySelector('ul').appendChild(li);
-
-    text.value = '';
-    text.focus();
+    
   });
 }
