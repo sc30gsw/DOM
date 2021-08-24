7c4c9160d44cc571d310933514a3dcd0ec2b4f2d
sc30gsw <haidoushi875@gmail.com>
2021-08-25 07:52:49 +0900

    準備

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    const item1 = document.querySelectorAll('li')[1];
-
-    // item1.remove();
-    document.querySelector('ul').removeChild(item1);
+    
   });
 }
