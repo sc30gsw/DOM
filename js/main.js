bd1016a714b6d86284a0652e78adef7bd0a31f67
sc30gsw <haidoushi875@gmail.com>
2021-08-25 08:00:03 +0900

    ToDoリスト作成
    input要素の操作

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    
+    const li = document.createElement('li');
+    const text = document.querySelector('input');
+    li.textContent = text.value;
+    document.querySelector('ul').appendChild(li);
+
+    text.value = '';
+    text.focus();
   });
 }
