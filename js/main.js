7c1c6a64cbd48e8647177642ad164e68a959816d
sc30gsw <haidoushi875@gmail.com>
2021-08-25 08:19:05 +0900

    準備

--- js/main.js
+++ js/main.js
@@ -2,17 +2,6 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    const colors = document.querySelectorAll('input');
-    let selectedColor;
-
-    colors.forEach(color => {
-      if (color.checked === true) {
-        selectedColor = color.value;
-      }
-    });
-
-    const li = document.createElement('li');
-    li.textContent = selectedColor;
-    document.querySelector('ul').appendChild(li);
+    
   });
 }
