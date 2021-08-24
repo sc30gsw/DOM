61ac91b6fbb723263e80613241d3c31ef85d25cc
sc30gsw <haidoushi875@gmail.com>
2021-08-25 07:39:01 +0900

    removeChild()の場合

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
   document.querySelector('button').addEventListener('click', () => {
     const item1 = document.querySelectorAll('li')[1];
 
-    item1.remove();
-
+    // item1.remove();
+    document.querySelector('ul').removeChild(item1);
   });
 }
