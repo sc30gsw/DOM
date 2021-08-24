1398a395a6991329737e14dab5d359ab40abc934
sc30gsw <haidoushi875@gmail.com>
2021-08-25 08:07:38 +0900

    セレクトボックスのToDoリスト作成

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    
+    const li = document.createElement('li');
+    const color = document.querySelector('select');
+    li.textContent = `${color.value} - ${color.selectedIndex}`;
+    document.querySelector('ul').appendChild(li);
   });
 }
