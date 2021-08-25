3731fcfee1acc45c5bfcb6ad270540f6a16ad880
sc30gsw <haidoushi875@gmail.com>
2021-08-26 07:46:17 +0900

    preventDefaultの設定

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 {
-  document.querySelector('form').addEventListener('submit', () => {
+  document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
     console.log('submit');
   });
 }
